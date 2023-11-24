a48aa3d73b950e5dffeffe2066d4c4deb2109a5c
Patryk Szewczyk <127959525+Patryk-Szewczyk@users.noreply.github.com>
2023-11-25 00:34:49 +0100

    Update code-maker.ts

--- code-maker.ts
+++ code-maker.ts
@@ -1,6 +1,6 @@
 console.log('e');
 
-const page_Obj: {
+const page_OBJ: {
     setPage: Function
 } = {
     setPage(): void {
@@ -13,4 +13,52 @@ const page_Obj: {
         });
     }
 };
-page_Obj.setPage();
+page_OBJ.setPage();
+
+const navMob_dropdownButton_OBJ: {
+    action: Function,
+    isEnable: boolean
+} = {
+    isEnable: false,
+    action() {
+        const button: HTMLDivElement = document.querySelector('div.nav-mob-dropdown-button');
+        const target: HTMLDivElement = document.querySelector('div.nav-mob-dropdown-target');
+        const strips_AR: any[] = [];
+        for (let i = 0; i < 3; i++ ) {
+            strips_AR[i] = document.querySelectorAll('div.nav-mob-dropdown-strip')[i];
+        };
+        console.log(strips_AR);
+        ['click', 'touchend'].forEach((ev) => {
+            button.addEventListener(ev, () => {
+                if (this.isEnable === false) {   // Show target:
+                    // Switch:
+                    this.isEnable = true;
+                    // Target:
+                    target.style.height = 500 + "px";
+                    target.style.transitionDuration = 0.35 + "s";
+                    // Strips:
+                    strips_AR[1].style.width = 0 + "px";
+                    strips_AR[1].style.transitionDuration = 0.175 + "s";
+                    strips_AR[0].style.transform = "rotate(45deg)";
+                    strips_AR[0].style.transitionDuration = 0.35 + "s";
+                    strips_AR[2].style.transform = "rotate(-45deg)";
+                    strips_AR[2].style.transitionDuration = 0.35 + "s";
+                } else if (this.isEnable === true) {  // Hide target:
+                    // Switch:
+                    this.isEnable = false;
+                    // Target:
+                    target.style.height = 0 + "px";
+                    target.style.transitionDuration = 0.35 + "s";
+                    // Strips:
+                    strips_AR[1].style.width = 30 + "px";
+                    strips_AR[1].style.transitionDuration = 0.175 + "s";
+                    strips_AR[0].style.transform = "rotate(0deg)";
+                    strips_AR[0].style.transitionDuration = 0.35 + "s";
+                    strips_AR[2].style.transform = "rotate(0deg)";
+                    strips_AR[2].style.transitionDuration = 0.35 + "s";
+                }
+            }, false);
+        });
+    }
+};
+navMob_dropdownButton_OBJ.action();
